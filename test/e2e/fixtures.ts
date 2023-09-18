11146b2cf2459120cb0edb24daef20a7918f89e3
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-09-18 12:13:14 +0900

    fix e2 tests

--- test/e2e/fixtures.ts
+++ test/e2e/fixtures.ts
@@ -1,6 +1,6 @@
 import { test as base } from '@playwright/test';
 import { username, password } from './consts.ts';
-// import { execSync } from 'child_process';
+
 import { addPasswordToUser, createUser } from '~/models/user.server.ts';
 import { resetDB } from 'test/utils.ts';
 
@@ -12,9 +12,11 @@ export const test = base.extend({
     await use(page);
   },
   loggedInPage: async ({ page }, use) => {
+    // TODO: login by setting session cookie directly instead of entering password
+    //       to test for users that only uses passkey
     const user = await createUser(username);
     await addPasswordToUser(user.id, password);
-    await page.goto('/login');
+    await page.goto('/login/password');
     await page.getByLabel(/username/i).fill(username);
     await page.getByLabel(/password/i).fill(password);
     await page.getByRole('button', { name: /log in/i }).click();
