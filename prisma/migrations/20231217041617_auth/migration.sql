5df86bf45569ae0758b318839b4d605ac4c04ebc
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-12-17 13:17:28 +0900

    reset migration

--- prisma/migrations/20231217041617_auth/migration.sql
+++ prisma/migrations/20231217041617_auth/migration.sql
@@ -9,15 +9,9 @@ CREATE TABLE `User` (
 
     UNIQUE INDEX `User_name_key`(`name`),
     UNIQUE INDEX `User_googleProfileId_key`(`googleProfileId`),
+    INDEX `user_google_profile_id`(`googleProfileId`),
+    INDEX `user_name`(`name`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Password` (
-    `hash` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `Password_userId_key`(`userId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -33,5 +27,7 @@ CREATE TABLE `Authenticator` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    INDEX `authenticator_credential_id`(`credentialID`),
+    INDEX `authenticator_user_id`(`userId`),
     PRIMARY KEY (`credentialID`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
